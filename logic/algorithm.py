2f4b6b7d4562ead5aa562bb2f3d66ab2b143dcd5
BorTink <borisus.amusov@mail.ru>
2024-05-13 13:27:57 +0300

    add actual ride keys

--- logic/algorithm.py
+++ logic/algorithm.py
@@ -7,6 +7,39 @@ import pytz
 from geocodes import Geocodes
 import dal
 
+a = {
+    'Дата заказа': 'date',  # Ночной развоз указан
+    'Время заказа': 'request_time',
+    'Рассчетное время подачи': 'arriving_time',
+    'Кто заказал': 'order_phone',
+    'Способ заказа': 'order_method',
+    'День недели завершения заказа': 'day_of_ride',
+    'Имя пользователя': 'username',
+    'Группа': 'ride_group',
+    'Актуальное подразделение': 'ssp_current',
+    'Подразделение сотрудника на момент заказа': 'ssp_during_order',
+    'Телефон': 'passenger_phone',
+    'Электронная почта': 'email',
+    'ID сотрудника': 'employee_id',
+    'Кому заказали: ФИО': 'passenger_pd',
+    'Идентификатор заказа': 'ride_id',
+    'Город': 'city',
+    'Адрес подачи': 'address_from',
+    'Промежуточные адреса': 'address_stay',
+    'Адрес назначения': 'address_to',
+    'Координаты подачи': 'coordinates_from',
+    'Координаты промежуточных адресов': 'coordinates_stay',
+    'Координаты адреса назначения': 'coordinates_to',
+    'Тариф': 'fare',  # Почему-то минус иногда
+    'Комментарий': 'commentary',
+    'Стоимость поездки': 'ride_cost',
+    'Стоимость ожидания': 'waiting_cost',
+    'Время ожидания в точке А': 'waiting_time_from',
+    'Время ожидания в промежуточных точках': 'waiting_time_stay',
+    'Цель поездки': 'ride_goal',
+    'Стоимость платной дороги': 'cost_of_toil_road'
+}
+
 grades_dict = {
     'Эконом': 1,
     'Комфорт': 2,
